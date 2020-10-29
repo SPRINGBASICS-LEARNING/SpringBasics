2773c71463fabcc52c31625821e6f4faf16f21ec
Tamminedi <vtammine@chivp122-3025030.ppp.ford.com>
2020-10-29 19:33:30 -0400

    Mixing XML Context with Component Scan for Beans defined with Annotations

--- src/main/java/com/learning/spring/basics/springin5steps/SpringXmlContextApplication.java
+++ src/main/java/com/learning/spring/basics/springin5steps/SpringXmlContextApplication.java
@@ -1,20 +1,20 @@
 package com.learning.spring.basics.springin5steps;
 
-import com.learning.spring.basics.springin5steps.basic.BinarySearchImpl;
 import com.learning.spring.basics.springin5steps.xml.XmlPersonDAO;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan
 public class SpringXmlContextApplication {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(SpringXmlContextApplication.class);
 
     public static void main(String[] args) {
 
         try(ClassPathXmlApplicationContext applicationContext =
                 new ClassPathXmlApplicationContext("applicationContext.xml")) {
+
+            LOGGER.info("Beans Loaded -> {}", (Object)applicationContext.getBeanDefinitionNames());
 
             XmlPersonDAO personDAO = applicationContext.getBean(XmlPersonDAO.class);
             System.out.println(personDAO);
